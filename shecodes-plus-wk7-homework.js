3d30cd8cb5b56288305a361145225288a8c18553
lisacodescom <alisalove896@gmail.com>
2023-07-12 18:56:50 -0500

    finish week 7 homework

--- shecodes-plus-wk7-homework.js
+++ shecodes-plus-wk7-homework.js
@@ -1,6 +1,5 @@
 function displayWeatherCondition(response){
-    let cityElement = document.querySelector("#city")
-    cityElement.innerHTML = response.data.name
+    document.querySelector("#city").innerHTML = response.data.name
     let temperatureElement = document.querySelector("#temperature")
     temperatureElement.innerHTML = response.data.main.temp
     let discriptionElement = document.querySelector("#discription")
@@ -10,7 +9,48 @@ function displayWeatherCondition(response){
     let windElement = document.querySelector("#wind");
     windElement.innerHTML = response.data.wind.speed
 }
+    
+    function searchCity(city){
+        let apiKey = "f2c2fbe1921af33c1d9afdc04239ba4f";
+        let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+        axios.get(apiUrl).then(displayWeatherCondition)
+    }
 
-let apiKey = "f2c2fbe1921af33c1d9afdc04239ba4f";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Dallas&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayWeatherCondition)
+    function handleSubmit(event) {
+      event.preventDefault();
+      let city = document.querySelector("#city-input").value;
+      searchCity(city);
+    }
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSubmit);
+
+
+function formatDate(date){
+    let hours = date.getHours()
+    if (hours < 10){
+        hours = `0${hours}`
+    }
+    let minutes = date.getMinutes()
+    if (minutes < 10){
+        minutes = `0${minutes}`
+    }
+    let dayIndex = date.getDay()
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ]
+    let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`
+}
+
+let dateElement = document.querySelector("#date");
+let currentTime = new Date();
+dateElement.innerHTML = formatDate(currentTime);
+
+searchCity("New York");
